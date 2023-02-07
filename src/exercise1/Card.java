b2ff63e5295378b9efa0a3274d267f3d6ea9f681
carlos <fierroap@sheridancollege.ca>
2023-02-07 00:40:30 -0500

    card trick solved

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -1,21 +1,27 @@
 package exercise1;
 
 /**
- * A class that models playing card Objects. Cards have 
- * a value (note that Ace = 1, Jack = 11, Queen = 12, King = 13)
- * A suit (clubs, hearts, spades, diamonds).
- * There are 52 cards in a deck, no jokers.
- * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ * A class that models playing card Objects. Cards have a value (note that Ace =
+ * 1, Jack = 11, Queen = 12, King = 13) A suit (clubs, hearts, spades,
+ * diamonds). There are 52 cards in a deck, no jokers. This code is to be used
+ * in ICE1. When you create your own branch, add your name as a modifier.
+ *
  * @author dancye
  * @author Paul Bonenfant May 2020
  */
 public class Card {
 
-   private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+    public static final String[] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    public final static int[] VALUE = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
-   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    protected int value;
+    protected String suit;
+
+    public Card(String suit, int rank) {
+        this.value = rank;
+        this.suit = suit;
+    }
+
     /**
      * @return the suit
      */
@@ -42,5 +48,21 @@ public class Card {
      */
     public void setValue(int value) {
         this.value = value;
-    }  
+    }
+
+    public boolean equals(Object tocompare) {
+        if (tocompare != null) {
+            Card cardtocompare = (Card) tocompare;
+
+            if (cardtocompare.suit.equals(this.suit) && cardtocompare.value == this.value) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public String toString() {
+        return "suit" + suit + "value" + value;
+    }
 }
--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,26 +1,61 @@
 package exercise1;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
- * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
- * It then searches the array of cards for the match to the user's card. 
- * To be used as starting code in Exercise
+ * A class that fills a hand of 7 cards with random Card Objects and then asks
+ * the user to pick a card. It then searches the array of cards for the match to
+ * the user's card. To be used as starting code in Exercise
  *
  * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Paul Bonenfant Jan 25, 2022
  */
 public class CardTrick {
-    
+
     public static void main(String[] args) {
-        
+
         Card[] hand = new Card[7];
 
+        Random rankgenerator = new Random();
+        Random suitgenerator = new Random();
+
+        System.out.println("pick a card");
+
+        Scanner pick = new Scanner(System.in);
+        int mypick = pick.nextInt();
+
+        System.out.println("pick a suit");
+
+        int mysuit = pick.nextInt();
+
         for (int i = 0; i < hand.length; i++) {
-            Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
+
+            String suits = Card.SUITS[suitgenerator.nextInt(4)];
+            int rank = Card.VALUE[rankgenerator.nextInt(13)];
+            
+            Card generatedcard = new Card(suits, rank);
+            
+
+            if (Arrays.asList(hand).contains(generatedcard)) {
+                i--;
+
+            } else {
+
+                System.out.println(new Card(suits, rank));
+                hand[i] = new Card(suits, rank);
+            }
+
             //       Don't worry about duplicates at this point
+        }
+        if (Arrays.asList(hand).contains(new Card(Card.SUITS[mysuit], Card.VALUE[mypick]))) {
+
+            System.out.println("card found");
+
+        } else {
+            System.out.println("not found");
+
         }
 
         // insert code to ask the user for Card value and suit, create their card
@@ -30,38 +65,29 @@ public class CardTrick {
         //       1 for Hearts, 2 for Diamonds, etc. (remember arrays are 0-based though)
         // 
         // Then loop through the cards in the array to see if there's a match.
-        
         // If the guess is successful, invoke the printInfo() method below.
-        
     }
-
-    /**
-     * A simple method to print out personal information. Follow the instructions to 
-     * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
-     */
-    private static void printInfo() {
-    
-        System.out.println("Congratulations, you guessed right!");
-        System.out.println();
-        
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
-        System.out.println();
-        
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
-	System.out.println();	
-
-        System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
-
-        System.out.println();
-        
-    
-    }
-
 }
+
+/**
+ * A simple method to print out personal information. Follow the instructions to
+ * replace this information with your own.
+ *
+ * @author Paul Bonenfant Jan 2022
+ */
+//private static void printInfo() {
+// System.out.println("Congratulations, you guessed right!");
+//System.out.println();
+//System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+//System.out.println();
+//System.out.println("My career ambitions:");
+//System.out.println("-- Be more active on LinkedIn");
+//System.out.println("-- Have a semester with no violations of academic integrity!");
+//System.out.println();	
+//System.out.println("My hobbies:");
+//System.out.println("-- Investing");
+//System.out.println("-- Cooking");
+//System.out.println("-- Reading/Watching TV");
+//System.out.println("-- Riding my motorcycle");
+//System.out.println(); 
+
